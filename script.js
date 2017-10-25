f3898eb05f48555d2e77050d7671e0fe8a35efb5
Pedro Crispim <pedro@loopsoftware.fr>
2017-10-25 15:18:08 +0100

    even more image stuff

--- script.js
+++ script.js
@@ -31,9 +31,22 @@ function gotMedia(mediaStream) {
     const imageCapture = new ImageCapture(mediaStreamTrack);
     imageCapture.takePhoto()
         .then(blob => createImageBitmap(blob))
-        .then(imageBitmap => { })
+        .then(imageBitmap => {
+            const canvas = document.querySelector("#photoCanvas");
+            drawCanvas(canvas, imageBitmap);
+        })
         .catch(error => console.error("gotMedia() error: ", error));
     console.log(imageCapture);
+}
+
+function drawCanvas(canvas, img) {
+    canvas.width = getComputedStyle(canvas).width.split('px')[0];
+    canvas.height = getComputedStyle(canvas).height.split('px')[0];
+    let ratio = Math.min(canvas.width / img.width, canvas.height / img.height);
+    let x = (canvas.width - img.width * ratio) / 2;
+    let y = (canvas.height - img.height * ratio) / 2;
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+    canvas.getContext('2d').drawImage(img, 0, 0, img.width, img.height, x, y, img.width * ratio, img.height * ratio);
 }
 
 function handleFileSelect(evt) {
