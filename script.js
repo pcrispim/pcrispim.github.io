5698189ecdae394e9a9acfe6a518cbb013687bc3
Pedro Crispim <pedro@loopsoftware.fr>
2017-11-03 14:54:33 +0000

    drawing base64 image

--- script.js
+++ script.js
@@ -44,9 +44,16 @@ function gotMedia(mediaStream) {
     console.log(imageCapture);
 }
 
-function showPhoto(image) {
+function showPhoto(imgBase64) {
     const canvas = document.querySelector("#photoCanvas");
-    drawCanvas(canvas, image);
+    const context = canvas.getContext("2d");
+
+    const image = new Image();
+    image.onload = () => {
+        context.drawImage(image, 0, 0);
+    };
+
+    image.src = imgBase64;
 }
 
 function drawCanvas(canvas, img) {
