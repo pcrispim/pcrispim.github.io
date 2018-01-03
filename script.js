251abfb96602c04dc6157923db6c159728279ec7
Pedro Crispim <pedro@loopsoftware.fr>
2018-01-03 15:44:50 +0000

    ...

--- script.js
+++ script.js
@@ -43,6 +43,8 @@ function getCamera() {
     document.location.replace("loop://camera");
 }
 
+function showPhoto()
+
 function gotMedia(mediaStream) {
     const mediaStreamTrack = mediaStream.getVideoTracks()[0];
     const imageCapture = new ImageCapture(mediaStreamTrack);
@@ -56,18 +58,58 @@ function gotMedia(mediaStream) {
     console.log(imageCapture);
 }
 
+window.URL = window.URL || window.webkitURL;
+
+var fileSelect = document.getElementById("fileSelect"),
+    fileElem = document.getElementById("fileElem"),
+    fileList = document.getElementById("fileList");
+
+fileSelect.addEventListener("click", function (e) {
+  if (fileElem) {
+    fileElem.click();
+  }
+  e.preventDefault(); // prevent navigation to "#"
+}, false);
+
+function handleFiles(files) {
+  if (!files.length) {
+    fileList.innerHTML = "<p>No files selected!</p>";
+  } else {
+    fileList.innerHTML = "";
+    var list = document.createElement("ul");
+    fileList.appendChild(list);
+    for (var i = 0; i < files.length; i++) {
+      var li = document.createElement("li");
+      list.appendChild(li);
+      
+      var img = document.createElement("img");
+      img.src = window.URL.createObjectURL(files[i]);
+      img.height = 60;
+      img.onload = function() {
+        window.URL.revokeObjectURL(this.src);
+      }
+      li.appendChild(img);
+      var info = document.createElement("span");
+      info.innerHTML = files[i].name + ": " + files[i].size + " bytes";
+      li.appendChild(info);
+    }
+  }
+}
+
 function handleCameraResult(imgBase64) {
-    alert("received image...");
 
-    const canvas = document.querySelector("#photoCanvas");
-    const context = canvas.getContext("2d");
 
-    const image = new Image();
-    image.onload = () => {
-        context.drawImage(image, 0, 0);
-    };
+    // alert("received image...");
 
-    image.src = imgBase64;
+    // const canvas = document.querySelector("#photoCanvas");
+    // const context = canvas.getContext("2d");
+
+    // const image = new Image();
+    // image.onload = () => {
+    //     context.drawImage(image, 0, 0);
+    // };
+
+    // image.src = imgBase64;
 }
 
 function showImage(imgBase64) {
