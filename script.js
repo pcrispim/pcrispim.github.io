ca3d9bd0b2780549b6c9f4cc4896630b37bd244c
Pedro Crispim <pedro@loopsoftware.fr>
2018-01-03 16:40:35 +0000

    uncommented camera handling

--- script.js
+++ script.js
@@ -63,51 +63,49 @@ var fileSelect = document.getElementById("fileSelect"),
     fileList = document.getElementById("fileList");
 
 fileSelect.addEventListener("click", function (e) {
-  if (fileElem) {
-    fileElem.click();
-  }
-  e.preventDefault(); // prevent navigation to "#"
+    if (fileElem) {
+        fileElem.click();
+    }
+    e.preventDefault(); // prevent navigation to "#"
 }, false);
 
 function handleFiles(files) {
-  if (!files.length) {
-    fileList.innerHTML = "<p>No files selected!</p>";
-  } else {
-    fileList.innerHTML = "";
-    var list = document.createElement("ul");
-    fileList.appendChild(list);
-    for (var i = 0; i < files.length; i++) {
-      var li = document.createElement("li");
-      list.appendChild(li);
-      
-      var img = document.createElement("img");
-      img.src = window.URL.createObjectURL(files[i]);
-      img.height = 60;
-      img.onload = function() {
-        window.URL.revokeObjectURL(this.src);
-      }
-      li.appendChild(img);
-      var info = document.createElement("span");
-      info.innerHTML = files[i].name + ": " + files[i].size + " bytes";
-      li.appendChild(info);
+    if (!files.length) {
+        fileList.innerHTML = "<p>No files selected!</p>";
+    } else {
+        fileList.innerHTML = "";
+        var list = document.createElement("ul");
+        fileList.appendChild(list);
+        for (var i = 0; i < files.length; i++) {
+            var li = document.createElement("li");
+            list.appendChild(li);
+
+            var img = document.createElement("img");
+            img.src = window.URL.createObjectURL(files[i]);
+            img.height = 60;
+            img.onload = function () {
+                window.URL.revokeObjectURL(this.src);
+            }
+            li.appendChild(img);
+            var info = document.createElement("span");
+            info.innerHTML = files[i].name + ": " + files[i].size + " bytes";
+            li.appendChild(info);
+        }
     }
-  }
 }
 
 function handleCameraResult(imgBase64) {
+    alert("received image...");
 
+    const canvas = document.querySelector("#photoCanvas");
+    const context = canvas.getContext("2d");
 
-    // alert("received image...");
+    const image = new Image();
+    image.onload = () => {
+        context.drawImage(image, 0, 0);
+    };
 
-    // const canvas = document.querySelector("#photoCanvas");
-    // const context = canvas.getContext("2d");
-
-    // const image = new Image();
-    // image.onload = () => {
-    //     context.drawImage(image, 0, 0);
-    // };
-
-    // image.src = imgBase64;
+    image.src = imgBase64;
 }
 
 function showImage(imgBase64) {
